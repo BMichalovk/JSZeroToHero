a425873f0c0784e22b1fc0ba880fb9664d950c9c
BMichalovk <bmichalovk@gmail.com>
2023-06-03 21:19:39 -0400

    Modal Project Completed

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const qsModal = document.querySelector('.modal');
+const qsOverlay = document.querySelector('.overlay');
+const qsCloseModal = document.querySelector('.close-modal');
+const qsButtonsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  qsModal.classList.remove('hidden');
+  qsOverlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  qsModal.classList.add('hidden');
+  qsOverlay.classList.add('hidden');
+};
+
+qsCloseModal.addEventListener('click', closeModal);
+qsOverlay.addEventListener('click', closeModal);
+qsButtonsOpenModal.forEach(element => {
+  element.addEventListener('click', openModal);
+});
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape' && !qsModal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
